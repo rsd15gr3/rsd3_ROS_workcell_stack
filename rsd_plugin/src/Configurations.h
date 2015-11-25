255985564ed46b33d9885003fa03ccde7b9cf0a8
SHJTurner <sttur14@student.sdu.dk>
2015-11-25 16:35:28 +0100

    added Configurations header file

---
+++ rsd_plugin/src/Configurations.h
@@ -0,0 +1,13 @@
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+#include <rw/math.hpp>
+using namespace rw::math;
+
+namespace Configurations
+{
+    const Q ConfigInit = Q(6,0.0,0.0,0.0,0.0,0.0,0.0);
+    const Q ConfigImgCapture = Q(6,1.420,0.679,0.360,0.699,-1.070,0.890);
+    const Q ConfigDropoff = Q(6,0.207,1.584,-0.738,0.000,-0.847,0.207);
+}
+#endif
