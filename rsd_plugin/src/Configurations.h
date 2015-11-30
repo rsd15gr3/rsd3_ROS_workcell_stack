010b2aee54f3030e56db2e986d0ab934734b1f30
SHJTurner <sttur14@student.sdu.dk>
2015-11-30 09:50:25 +0100

    updated dropoff config

--- rsd_plugin/src/Configurations.h
+++ rsd_plugin/src/Configurations.h
@@ -8,7 +8,7 @@ namespace Configurations
 {
     const Q ConfigInit = Q(6,0.0,0.0,0.0,0.0,0.0,0.0);
     const Q ConfigImgCapture = Q(6,1.420,0.679,0.360,0.699,-1.070,0.890);
-    const Q ConfigDropoff = Q(6,0.125,1.557,-0.678,0.000,-0.880,0.126);
+    const Q ConfigDropoff = Q(6,0.125,1.557,-0.679,-0.002,-0.879,0.125);
     //const Q ConfigDropoff = Q(6,0.207,1.584,-0.738,0.000,-0.847,0.207); //OLD
 }
 #endif
